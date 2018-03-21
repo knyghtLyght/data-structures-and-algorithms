c29b7e88cba43ab39972bb076e39272d230a4f88
Kevin <kevnaf@gmail.com>
2018-03-20 22:38:50 -0700

    fist pass

---
+++ Challenges/shift-array/shift-array/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace shift_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] test = new int[] { 1, 2, 3, 4, 5 };
+            int insert = 8;
+            int[] newArray = ShiftArray(test, insert);
+            for (int i = 0; i < test.Length; i++)
+            {
+                Console.WriteLine(test[i]);
+            }
+            for (int i = 0; i < newArray.Length; i++)
+            {
+                Console.WriteLine(newArray[i]);
+            }
+            Console.ReadLine();
+        }
+
+        static int[] ShiftArray (int[] input, int valueToAdd)
+        {
+            int startLength = input.Length;
+            int[] returnArray = new int[startLength + 1];
+            int midPoint = startLength / 2;
+            for (int i = 0; i < startLength-1; i++)
+            {
+                if (i < midPoint)
+                {
+                    returnArray[i] = input[i];
+                }
+                if (i == midPoint)
+                {
+                    returnArray[i] = valueToAdd;
+                    returnArray[i+1] = input[i];
+                }
+                if (i > midPoint)
+                {
+                    returnArray[i + 1] = input[i];
+                }
+            }
+            return returnArray;
+        }
+    }
+}
