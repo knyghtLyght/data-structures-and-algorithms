6a911ae3ab1531bac53c3b60f3d913a3939cdf21
Kevin Farrow <kevnaf@gmail.com>
2018-03-21 13:51:05 -0700

    whiteboard 3 first pass

---
+++ Challenges/binary-search/ReadMe.md
@@ -0,0 +1,8 @@
+# Binary Search
+A whieboard challenege intended to help us understand data structures. 
+
+## Challenge
+Write a function called BinarySearch which takes in 2 parameters: a sorted array and the search key. Without utilizing any of the built-in methods available to your language, return the index of the array’s element that is equal to the search key, or -1 if the element does not exist.
+
+## Solution
+![binary-serch](../../assets/binary-serch.jpg)
--- Challenges/binary-search/binary-search/Program.cs
+++ Challenges/binary-search/binary-search/Program.cs
@@ -2,11 +2,25 @@
 
 namespace binary_search
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] test = new int[] { 1, 2, 3, 4, 5 };
+            Console.WriteLine(Search(test, 3));
+            Console.ReadLine();
+        }
+
+        public static int Search(int[] input, int key)
+        {
+            for (int i = 0; i <= input.Length-1; i++)
+            {
+                if (input[i] == key)
+                {
+                    return i;
+                }
+            }
+            return -1;
         }
     }
 }
