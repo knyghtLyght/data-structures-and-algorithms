3425c78095d08d03e510e31929605e377ef43c9e
Kevin <kevnaf@gmail.com>
2018-03-25 21:25:25 -0700

    linked list first pass

---
+++ Challenges/Linked_List/Linked_List/LinkedList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linked_List
+{
+    class LinkedList
+    {
+
+        // The list is initially empty.
+        private Node head = null;
+
+        // Add a node at the beginning of the list with t as its data value.
+        public void AddNode(LinkedList t)
+        {
+            Node newNode = new Node();
+            newNode.Next = head;
+            newNode.Value = 0;
+            head = newNode;
+        }
+    }
+}
---
+++ Challenges/Linked_List/Linked_List/Node.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linked_List
+{
+    class Node
+    {
+        // The node holds an int value
+        public int Value { get; set; }
+        // Every node should have a refferance to the next node
+        public Node Next { get; set; }
+    }
+}
