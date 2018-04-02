b716cd397fa5ca500bd5123638831a4e6dafb1dc
Kevin Farrow <kevnaf@gmail.com>
2018-04-02 14:01:06 -0700

    First pass

---
+++ Challenges/queue-with-stacks/queue-with-stacks/Node.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace queue_with_stacks
+{
+    public class Node
+    {
+        public Node Next { get; set; }
+        public int Value { get; set; }
+    }
+}
---
+++ Challenges/queue-with-stacks/queue-with-stacks/StackedQueue.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace queue_with_stacks
+{
+    public class StackedQueue
+    {
+        public static Node SeedNode { get; set; }
+
+        public StackedQueue( int value)
+        {
+            SeedNode.Value = value;
+        }
+
+        MyStack inputStack = new MyStack(SeedNode);
+        MyStack outputStack = new MyStack(SeedNode);
+
+        public void Enqueue(int value)
+        {
+            Node node = new Node() { Value = value };
+            inputStack.Push(node);
+        }
+
+        public int Dequeue()
+        {
+            if (outputStack == null)
+            {
+                while (inputStack != null)
+                {
+                    outputStack.Push(inputStack.Pop());
+                }
+            }
+            return outputStack.Pop().Value;
+        }
+    }
+}
