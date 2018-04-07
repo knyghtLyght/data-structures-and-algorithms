018d8e77ee545313ac6d428e061212ff12521eaf
Kevin Farrow <kevnaf@gmail.com>
2018-04-06 19:36:52 -0700

    methods on the right class now

--- Implementations/BinaryTree/BinaryTree/Node.cs
+++ Implementations/BinaryTree/BinaryTree/Node.cs
@@ -9,49 +9,5 @@ namespace BinaryTree
         public int Value { get; set; }
         public Node LeftChild { get; set; }
         public Node RightChild { get; set; }
-
-        public void Add(Node node)
-        {
-
-        }
-
-        public void InOrder(Node node)
-        {
-            if (node.LeftChild != null)
-            {
-                InOrder(node.LeftChild);
-            }
-            Console.WriteLine(node.Value);
-            if (node.RightChild != null)
-            {
-                InOrder(node.RightChild);
-            }
-        }
-
-        public void PreOrder(Node node)
-        {
-            if (node.LeftChild != null)
-            {
-                InOrder(node.LeftChild);
-            }
-            if (node.RightChild != null)
-            {
-                InOrder(node.RightChild);
-            }
-            Console.WriteLine(node.Value);
-        }
-
-        public void PostOrder(Node node)
-        {
-            Console.WriteLine(node.Value);
-            if (node.LeftChild != null)
-            {
-                InOrder(node.LeftChild);
-            }
-            if (node.RightChild != null)
-            {
-                InOrder(node.RightChild);
-            }
-        }
     }
 }
