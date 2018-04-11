e6a561f0c0015097b46c9011352d37649b454295
Kevin Farrow <kevnaf@gmail.com>
2018-04-11 14:12:15 -0700

    scafold

---
+++ Challenges/find_maximum_value_binary_tree/find_maximum_value_binary_tree/MyTree.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace find_maximum_value_binary_tree
+{
+    public class MyTree
+    {
+        public static Node Root { get; set; }
+        public int MaxValue { get; set; }
+
+        public MyTree()
+        {
+            Root = null;
+        }
+
+        public void Add(Node add)
+        {
+
+        }
+
+        public 
+
+        public void InOrder(Node node)
+        {
+            if (node.LeftChild != null)
+            {
+                InOrder(node.LeftChild);
+            }
+            Console.WriteLine(node.Value);
+            if (node.RightChild != null)
+            {
+                InOrder(node.RightChild);
+            }
+        }
+
+        public void PreOrder(Node node)
+        {
+            Console.WriteLine(node.Value);
+            if (node.LeftChild != null)
+            {
+                InOrder(node.LeftChild);
+            }
+            if (node.RightChild != null)
+            {
+                InOrder(node.RightChild);
+            }
+        }
+
+        public void PostOrder(Node node)
+        {
+            if (node.LeftChild != null)
+            {
+                InOrder(node.LeftChild);
+            }
+            if (node.RightChild != null)
+            {
+                InOrder(node.RightChild);
+            }
+            Console.WriteLine(node.Value);
+        }
+    }
+}
