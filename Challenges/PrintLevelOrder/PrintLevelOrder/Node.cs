e3b32b4ad1af5d5e7b57b47df554997c7a395ce8
Kevin Farrow <kevnaf@gmail.com>
2018-04-24 13:42:11 -0700

    preclass commit

---
+++ Challenges/PrintLevelOrder/PrintLevelOrder/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrintLevelOrder
+{
+    public class Node
+    {
+        public int Value { get; set; }
+        public Node Next { get; set; }
+        public List<Node> Children { get; set; }
+
+        public Node()
+        {
+            Children = new List<Node>();
+        }
+    }
+}
--- Challenges/PrintLevelOrder/PrintLevelOrder/Program.cs
+++ Challenges/PrintLevelOrder/PrintLevelOrder/Program.cs
@@ -6,7 +6,30 @@ namespace PrintLevelOrder
     {
         static void Main(string[] args)
         {
-            
+            MyKTree testTree = new MyKTree(new Node() { Value = 10 });
+            testTree.Add(10, 9);
+            testTree.Add(10, 12);
+            testTree.Add(10, 8);
+            testTree.Add(12, 13);
+            testTree.Add(12, 15);
+            testTree.Add(12, 19);
+            testTree.Add(9, 5);
+            testTree.Add(10, 9);
+            testTree.Add(5, 2);
+        }
+
+        public static string PrintLevelOrder(MyKTree input)
+        {
+            string response = "";
+            MyQueue stepQueue = new MyQueue(input.Root);
+            Node runner = input.Root;
+            int currentLevel = 0;
+            int countTwo = 0;
+            while (runner != null)
+            {
+
+            }
+            return response;
         }
     }
 }
