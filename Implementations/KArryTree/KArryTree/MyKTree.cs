ab13a92aa29db5e2053ea484cf796d6e47424a8e
Kevin Farrow <kevnaf@gmail.com>
2018-04-25 10:59:22 -0700

    added comments

--- Implementations/KArryTree/KArryTree/MyKTree.cs
+++ Implementations/KArryTree/KArryTree/MyKTree.cs
@@ -15,31 +15,33 @@ namespace KArryTree
 
         public string BreadthFirst()
         {
-            MyQueue stepQueue = new MyQueue(Root);
+            MyQueue stepQueue = new MyQueue(Root); // Init the traversal queue
             Node runner = Root;
             string response = "";
-            while (stepQueue.Peek() != null)
+            while (stepQueue.Peek() != null) //while the queue has something in it keep going
             {
                 foreach (Node child in runner.Children)
                 {
                     stepQueue.Enqueue(child);
                 }
-                response += $"{stepQueue.Dequeue().Value} ";
-                runner = stepQueue.Front;
+                response += $"{stepQueue.Dequeue().Value} "; //Append the response string
+                runner = stepQueue.Front; //Move the runner
             }
             return response;
         }
 
         public Node Find(int targetNodeValue)
         {
-            MyQueue stepQueue = new MyQueue(Root);
+            MyQueue stepQueue = new MyQueue(Root); //Init traversal queue
             Node runner = Root;
-            while (stepQueue.Peek() != null)
+            while (stepQueue.Peek() != null) //While there is somethign in the queue keep going
             {
+                //If we found what we want break
                 if (runner.Value == targetNodeValue)
                 {
                     return runner;
                 }
+                //Add the node children to the traversal queue
                 if (runner.Children != null)
                 {
                     foreach (Node child in runner.Children)
@@ -47,14 +49,15 @@ namespace KArryTree
                         stepQueue.Enqueue(child);
                     }
                 }
-                stepQueue.Dequeue();
-                runner = stepQueue.Front;
+                stepQueue.Dequeue(); //drop the last checked node
+                runner = stepQueue.Front; //Move the runner
             }
             return new Node();
         }
 
         public void Add(int targetNodeValue, int valueToAdd)
         {
+            //find our node with the desired value and add to its children
             Find(targetNodeValue).Children.Add(new Node() { Value = valueToAdd });
         }
     }
