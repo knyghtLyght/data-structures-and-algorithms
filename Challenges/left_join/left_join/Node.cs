656d9a0f63e04783ec26052d3fee91ebe08eb287
Kevin Farrow <kevnaf@gmail.com>
2018-05-02 15:53:34 -0700

    eod commit

---
+++ Challenges/left_join/left_join/Node.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace left_join
+{
+    public class Node
+    {
+        public string Value { get; set; }
+        public string Key { get; set; }
+        public Node Next { get; set; }
+    }
+}
---
+++ Challenges/left_join/left_join/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace left_join
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            MyHashTable testTableOne = new MyHashTable();
+            testTableOne.Add("fond", "enamored");
+            testTableOne.Add("wrath", "anger");
+            testTableOne.Add("diligent", "employed");
+            testTableOne.Add("outfit", "garb");
+            testTableOne.Add("guide", "usher");
+
+            MyHashTable testTableTwo = new MyHashTable();
+            testTableTwo.Add("fond", "adverse");
+            testTableTwo.Add("wrath", "delight");
+            testTableTwo.Add("diligent", "idle");
+            testTableTwo.Add("guide", "follow");
+            testTableTwo.Add("flow", "jam");
+        }
+
+        public static MyHashTable LeftJoin(MyHashTable tableOne, MyHashTable tableTwo)
+        {
+            List<string> returnTable = new List<string>();
+            foreach (LinkedList<Node> item in tableOne.Table)
+            {
+                string key = item.First.Value.Key;
+                if (tableTwo.Contains(key) != null)
+                {
+                    
+                }
+            }
+        }
+    }
+}
