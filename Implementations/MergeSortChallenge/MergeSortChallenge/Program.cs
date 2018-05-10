e40208f9c86bf5ec1d98ba17a1e5c4f8e38f14d5
Kevin <kevnaf@gmail.com>
2018-05-09 22:43:05 -0700

    logic and test done

---
+++ Implementations/MergeSortChallenge/MergeSortChallenge/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace MergeSortChallenge
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] testArray = new int[10] { 4, 3, 5, 2, 6, 1, 7, 9, 8, 10 };
+            foreach (int item in testArray)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+            int[] result = MergeSort(testArray);
+
+            foreach (int item in result)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+
+        public static int[] MergeSort(int[] input)
+        {
+            MergeSort(input, 0, input.Length - 1);
+            return input;
+        }
+
+        public static void MergeSort(int[] input, int low, int high)
+        {
+            if (low < high)
+            {
+                int middle = (low / 2) + (high / 2);
+                MergeSort(input, low, middle);
+                MergeSort(input, middle + 1, high);
+                Merge(input, low, middle, high);
+            }
+        }
+
+        private static void Merge(int[] input, int low, int middle, int high)
+        {
+
+            int left = low;
+            int right = middle + 1;
+            int[] tmp = new int[(high - low) + 1];
+            int tmpIndex = 0;
+
+            while ((left <= middle) && (right <= high))
+            {
+                if (input[left] < input[right])
+                {
+                    tmp[tmpIndex] = input[left];
+                    left = left + 1;
+                }
+                else
+                {
+                    tmp[tmpIndex] = input[right];
+                    right = right + 1;
+                }
+                tmpIndex = tmpIndex + 1;
+            }
+
+            if (left <= middle)
+            {
+                while (left <= middle)
+                {
+                    tmp[tmpIndex] = input[left];
+                    left = left + 1;
+                    tmpIndex = tmpIndex + 1;
+                }
+            }
+
+            if (right <= high)
+            {
+                while (right <= high)
+                {
+                    tmp[tmpIndex] = input[right];
+                    right = right + 1;
+                    tmpIndex = tmpIndex + 1;
+                }
+            }
+
+            for (int i = 0; i < tmp.Length; i++)
+            {
+                input[low + i] = tmp[i];
+            }
+        }
+    }
+}
